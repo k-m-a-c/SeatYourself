2582ea36c3f0039e812a0d5e50c99ceb96c3ea5d
Michael Fich <michaeltf@gmail.com>
2015-07-02 15:33:29 -0400

    Added the Sessions controller and routes to the project

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create]
   resources :restaurants, only:  [:index, :show, :new, :create]
   resources :reservations, only: [:show, :new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
