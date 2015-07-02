c04cc5ffb3f85feb354dad29f553dab6ec80cfb5
Michael Fich <michaeltf@gmail.com>
2015-07-02 16:21:10 -0400

    Added log in and log out functions.  Going for a coffee

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      flash[:notice] = "Successfully logged in!"
+      session[:user_id] = user.id
+      redirect_to :root
+    else
+      flash[:alert] = "Wrong email address or password.  Try again."
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out."
+    redirect_to :root
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'users#new'
-
   resources :users, only: [:show, :new, :create]
   resources :restaurants, only:  [:index, :show, :new, :create]
   resources :reservations, only: [:show, :new, :create, :destroy]
