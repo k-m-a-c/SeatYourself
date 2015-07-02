2b03b5710142e7c1fc87985d38489d75069d5eb7
Michael Fich <michaeltf@gmail.com>
2015-07-02 15:30:38 -0400

    Fixed Kevin's spelling errors in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
-  resrouces :users, only: [:show, :new, :create]
+  resources :users, only: [:show, :new, :create]
   resources :restaurants, only:  [:index, :show, :new, :create]
   resources :reservations, only: [:show, :new, :create, :destroy]
-
 end
