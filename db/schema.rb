f469004b46bf2d1f8e3dcc7f592e6993371ac110
Michael Fich <michaeltf@gmail.com>
2015-07-06 17:27:49 -0400

    Remove timezones from restaurants.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706194116) do
+ActiveRecord::Schema.define(version: 20150706212538) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20150706194116) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "owner_id"
-    t.string   "timezone"
   end
 
   create_table "users", force: :cascade do |t|
